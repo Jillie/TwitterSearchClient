8924ac3d286b86caa8a21353e258fd7d0fc2cf69
Jill Mongato <jill.mongato@gmail.com>
2012-11-02 21:53:31 -0400

    first part of twitter client

--- twitter.py
+++ twitter.py
@@ -1,1 +1,21 @@
 #twitter.py
+
+import urllib
+import json
+import sys
+
+def search_twitter (query='python'):
+	url = 'http://search.twitter.com/search.json?q=' + query
+	response = urllib.urlopen(url).read()
+	data = json.loads(response)
+	return data['results']
+	
+def print_tweets(tweets):
+	for tweet in tweets:
+		print tweet['from_user'] + ': ' + tweet['text'] + '\n'
+
+if __name__ == "__main__":
+	query = sys.argv[1]
+	results = search_twitter(query)
+	print_tweets(results)
+	
